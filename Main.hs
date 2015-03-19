41cfcd09377636d7e1e6602b254cec6615f17d16
Izaak Meckler <ihmeckler@gmail.com>
2015-03-18 23:22:55 -0500

    deleted comments

--- Main.hs
+++ Main.hs
@@ -2,15 +2,8 @@ module Main where
 
 import System.Environment (getArgs)
 import Distribution.Simple.Configure (getConfigStateFile)
--- import Distribution.PackageDescription.Parse
--- import Distribution.Verbosity (silent)
--- import Data.Generics.Text
-
-x :: Int
-x = _
 
 main = do
   (path:_) <- getArgs
   print =<< getConfigStateFile path
---  pkgDesc <- readPackageDescription silent path
 
